87a5810d7f4b37e799e413e6a3bd35ad9ef87af1
TommyJams <nikhilk218@gmail.com>
2013-09-15 13:08:34 +0530

    config changes 2

--- application/config/constants.php
+++ application/config/constants.php
@@ -87,7 +87,5 @@ define('RADIO_FORM_MSG_API_FAILURE','Sorry. Try again later.');
 define('RADIO_FORM_MSG_DATABASE_INSERT_FAILURE','Sorry. Could not insert into database.');
 define('RADIO_FORM_SEND_MSG_OK','Thank you. Your request has been submitted! You will receive an email from us shortly.');
 
-//define('DATABASE','tommyjam_test');
-
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
--- application/config/database.php
+++ application/config/database.php
@@ -1,4 +1,10 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+if (is_readable(FCPATH . 'config.local.php'))
+{
+    include_once(FCPATH . 'config.local.php');
+}
+
 /*
 | -------------------------------------------------------------------
 | DATABASE CONNECTIVITY SETTINGS
@@ -48,11 +54,11 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'tommyjamstest.cloudapp.net';
-$db['default']['username'] = 'tommyjamslinux';
-$db['default']['password'] = '1tommyblah';
-$db['default']['database'] = 'tommyjam_test';
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['hostname'] = DATABASE_HOSTNAME;
+$db['default']['username'] = DATABASE_USERNAME;
+$db['default']['password'] = DATABASE_PASSWORD;
+$db['default']['database'] = DATABASE;
+$db['default']['dbdriver'] = DATABASE_DRIVER;
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
