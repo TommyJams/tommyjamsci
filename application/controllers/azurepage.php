0d3040e0f4fd669412e8f68c065e3db93a27f859
TommyJams <tommyjams.bizspark@outlook.com>
2013-09-15 06:40:48 +0530

    azure page 1

--- application/controllers/azurepage.php
+++ application/controllers/azurepage.php
@@ -3,16 +3,11 @@
 class Azurepage extends CI_Controller{
 
 	public function index(){
-		// echo "You are in index";
-		// $this->addstuff();
 		 $this->azurelanding();
 	}
 
-
 	public function azurelanding(){
-		// echo "this is you";
 		$this->load->view('azure_view');
 	}
-
 }
 ?>
